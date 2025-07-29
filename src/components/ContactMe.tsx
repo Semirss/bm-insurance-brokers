96d31ff4bf6bab04c1e3032fd5b5c4d8c9f5da7f
Semir_S <149691609+Semirss@users.noreply.github.com>
2025-07-29 10:28:12 +0300

    hy

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -89,8 +89,8 @@ const ContactMe: React.FC = () => {
                 <div className="flex items-center text-gray-800">
                   <svg className="w-6 h-6 text-blue-600 mr-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8m-2 4v7a2 2 0 01-2 2H5a2 2 0 01-2-2v-7"></path></svg>
                   <div>
-                    <p className="font-semibold">Email Us</p>
-                    <p>contact@bminsurancebrokers.com</p>
+                    <p className="font-semibold ">Email Us</p>
+                    <p>@bminsurancebrokers.com</p>
                   </div>
                 </div>
                 <div className="flex items-center text-gray-800">
